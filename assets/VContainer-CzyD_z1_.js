d733f7591036fd53a0e9bc2749688733f8a020e6
kwccc073 <164137605+kwccc073@users.noreply.github.com>
2024-08-26 03:38:28 +0000

    Deploying to gh-pages from @ kwccc073/GrooveShare@afbdd41da55c5c8227dc026f0f1ad5a2049aa27b 🚀

--- assets/VContainer-CzyD_z1_.js
+++
@@ -1,1 +0,0 @@
-import{p as n,m as o,a as r,g as l,n as c,u as i,c as u}from"./index-Dl0isM8a.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=c();return i(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{C as V};
